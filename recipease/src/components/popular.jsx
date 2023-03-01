048ea1c79e6452f7cf61f4611745c6e002ac2151
Graeme Paterson <ghpaterson@gmail.com>
2023-03-01 17:53:43 +0000

    getPopular function created and rendering the title from the recipes

--- recipease/src/components/popular.jsx
+++ recipease/src/components/popular.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 
 function Popular() {
+
+const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API}&number=8`);
+    const data = await api.json();
+    setPopular(data.recipes)
+    console.log(data)
+  }
   return (
-    <div>Popular</div>
+    <div>
+      {popular.map((recipe) => {
+        return(
+          <div>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
   )
 }
 
