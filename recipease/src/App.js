07d96a8c7a647b97036f1ccbd4f89dd9025ac659
Graeme Paterson <ghpaterson@gmail.com>
2023-03-02 17:41:18 +0000

    Search component created and initial search bar styled

--- recipease/src/App.js
+++ recipease/src/App.js
@@ -1,11 +1,13 @@
 import Pages from './pages/pages'
 import Category from './components/category';
 import {BrowserRouter} from 'react-router-dom'
+import Search from './components/search';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
