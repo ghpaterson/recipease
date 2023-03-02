3f3dd479c5c2adb12d02c0db964000a7fe685dd7
Graeme Paterson <ghpaterson@gmail.com>
2023-03-02 15:38:13 +0000

    StyledLinks for cuisine

--- recipease/src/components/category.jsx
+++ recipease/src/components/category.jsx
@@ -6,22 +6,22 @@ import React from 'react'
 function Category() {
   return (
     <List>
-        <NavLink to={'cuisine/Mexican'}>
+        <StyledLink to={'cuisine/Mexican'}>
             <GiTacos />
             <h4>Mexican</h4>
-        </NavLink>
-        <NavLink to={'cuisine/Asian'}>
+        </StyledLink>
+        <StyledLink to={'cuisine/Asian'}>
             <GiNoodles />
             <h4>Asian</h4>
-        </NavLink>
-        <NavLink to={'cuisine/Italian'}>
+        </StyledLink>
+        <StyledLink to={'cuisine/Italian'}>
             <GiFullPizza />
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'cuisine/British'}>
+        </StyledLink>
+        <StyledLink to={'cuisine/British'}>
             <GiFlatfish />
             <h4>British</h4>
-        </NavLink>
+        </StyledLink>
     </List>
   )
 }
@@ -30,6 +30,42 @@ const List = styled.div`
   display: flex;
   justify-content: center;
   margin: 2rem 0rem;    
+`;
+
+const StyledLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+  }
+
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
+  &.active{
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    svg {
+      color: white;
+    }
+
+    h4 {
+      color: white;
+    }
+  }
 `;
 
 export default Category
