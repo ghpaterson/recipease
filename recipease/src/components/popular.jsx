bd3e903e3e44793467fe11494495c314d76f0f24
Graeme Paterson <ghpaterson@gmail.com>
2023-03-02 11:53:18 +0000

    Card styling - recipe title inside each card

--- recipease/src/components/popular.jsx
+++ recipease/src/components/popular.jsx
@@ -21,13 +21,20 @@ const [popular, setPopular] = useState([]);
     <div>
           <Wrapper>
             <h3>Trending Recipes</h3>
-            <Splide>
+            <Splide options={{
+              perPage: 4,
+              arrows: false, 
+              pagination: false,
+              drag: "free",
+              gap: "3rem",
+            }}>
             {popular.map((recipe) => {
               return(
                 <SplideSlide>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
                 </Card>
                 </SplideSlide>
               );
@@ -46,10 +53,40 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
-  img{
+  img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1.5rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 export default Popular
