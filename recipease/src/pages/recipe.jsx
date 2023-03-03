2653ababe58f52c72f877bdc7678886f1c810e02
Graeme Paterson <ghpaterson@gmail.com>
2023-03-03 09:35:36 +0000

    recipe styling started

--- recipease/src/pages/recipe.jsx
+++ recipease/src/pages/recipe.jsx
@@ -22,8 +22,53 @@ function Recipe() {
 
 
   return (
-    <div>{details.title}</div>
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt=""/>
+      </div>
+      <Info>
+        <Button>Ingredients</Button>
+        <Button>Method</Button>
+      </Info>
+    </DetailWrapper>
   )
 }
+
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #36032a, #21011a);
+    color: white;
+  }
+
+  h2 {
+    margin-bottom: 2rem;
+  }
+
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #36032a;
+  background: white;
+  border: 2px #313131;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  margin-left: 10rem;
+`;
 
 export default Recipe;
