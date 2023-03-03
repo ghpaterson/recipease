03a680afd3f7cce36009690bb81a01c637e556b0
Graeme Paterson <ghpaterson@gmail.com>
2023-03-03 09:52:04 +0000

    rendered out summary, method, and ingredients - not formatted

--- recipease/src/pages/recipe.jsx
+++ recipease/src/pages/recipe.jsx
@@ -36,6 +36,15 @@ function Recipe() {
         <Button className={activeTab === 'method' ? 'active' : ''} onClick={() => setActiveTab("method")}>
           Method
         </Button>
+        <div>
+          <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+          <h3 dangerouslySetInnerHTML={{__html: details.instructions}}></h3>
+        </div>
+        <ul>
+          {details.extendedIngredients.map((ingredient) => (
+            <li key={ingredient.id}>{ingredient.original}</li>
+          ))}
+        </ul>
       </Info>
     </DetailWrapper>
   )
